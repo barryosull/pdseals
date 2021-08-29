2bf9477369fb3fe9365822972d68c71b274d392d
Barry O Sullivan <barry@tercet.io>
2021-08-29 18:53:32 +0100

    Now parsing the fixture file and extracing the JSON.

---
+++ README.md
@@ -0,0 +1,7 @@
+Start of the project. 
+
+I have it finding and loading the JS on the page.
+
+Next steps:
+Parse out the data and get the product name, then prices.
+
--- src/Parser.php
+++ src/Parser.php
@@ -1,11 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Barryosull\PSDeals;
+
+use Symfony\Component\DomCrawler\Crawler;
 
 class Parser
 {
     public function parse(string $html): \stdClass
     {
-        return (object)[];
+        $crawler = new Crawler($html);
+
+        $jsonNode = $crawler->filter('#__NEXT_DATA__');
+
+        $json = $jsonNode->text();
+
+        $object =  json_decode($json, false);
+
+        return $object->props->apolloState;
     }
 }
